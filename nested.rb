223ff1068a4c4b4fc4960277dd65c425899e229b
Le H Ngo <seiji@o2.pl>
2019-05-23 11:23:13 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -66,8 +66,6 @@ def adding_matz
 #    :languages => ["LISP", "C"]
 # }
 
-
-
 	programmer_hash =
  		{
         :grace_hopper => {
@@ -84,8 +82,13 @@ def adding_matz
         }
      }
 
-programmer_hash[:yukihiro_matsumoto] = {}
+programmer_hash[:yukihiro_matsumoto] = {
+																				:known_for => "Ruby",
+																				:languages => ["LISP", "C"]
+																			}
 
+
+programmer_hash[:yukihiro_matsumoto]
 end
 
 def changing_alan
